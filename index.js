2d8aaa6e3ddedaf6b5ab743e6d543ad165f4162f
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-05-30 23:16:32 -0400

    visualizing time complexities notes

--- index.js
+++ index.js
@@ -17,13 +17,25 @@
 // console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`)
 
 // Example 2
-function addUpTo(n) {
-    return n * (n + 1) / 2
-}
+// function addUpTo(n) {
+//     return n * (n + 1) / 2
+// }
 
 // console.log(addUpTo(6))
 
-let t1 = performance.now()
-addUpTo(1000000000)
-let t2 = performance.now()
-console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`)
+// let t1 = performance.now()
+// addUpTo(1000000000)
+// let t2 = performance.now()
+// console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds`)
+
+// Visualizing Time Complexities
+
+function addUpToFirst(n) {
+    var total = 0;
+    for (var i = 0; i <= n; i++) {
+      total += i;
+    }
+    return total;
+  }
+
+  console.log(addUpToFirst(10))
